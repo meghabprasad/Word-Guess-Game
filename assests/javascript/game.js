9b806404b0578ff9ec32a2bf6ace392e1df7bec2
Megha Prasad Bindiganavale <Megha@calvisitor-10-105-192-128.calvisitor.1918.berkeley.edu>
2019-05-22 10:17:05 -0700

    create pseudocode in jc file

---
+++ assests/javascript/game.js
@@ -0,0 +1,25 @@
+// theme: nba teams, desserts, countries, tv shows, colors, friends characters 
+// create array of word options "Algeria, America, Australia, Brazil"
+// create a counter called wins set to 0 
+// create a counter called loses set to 0
+// create a counter for guessRemaining and set it to 5;
+// create an array that has the lettersGuessed 
+// for the length of the word, create _ at each index (loop)
+// display this ___ 
+// get user guess 
+    // onkeyup and save this key 
+    // compare this key with the letters of the word 
+    // if that letter exists, replace _ with the letter 
+    // push the letter to the lettersGuessed array
+// display the incomplete word __y_
+
+var wordOptions = ["Algeria", "America", "Australia", "Brazil", "Mexico"];
+var wins = 0;
+var loses = 0;
+var guessesRemaining = 5;
+var lettersGuessed = [];
+
+//press any key to get started 
+document.onkeyup = function (event){
+    
+}
